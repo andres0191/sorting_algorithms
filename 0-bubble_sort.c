c7afbb0717e9c5554fe72dafa5f2814a6d4ff500
andres0191 <andres_felipe0191@hotmail.com>
2020-01-30 22:58:47 +0000

    add file of task number 0 and the main of the task

---
+++ 0-bubble_sort.c
@@ -0,0 +1,13 @@
+#include "sort.h"
+/**
+* bubble_sort - Function that sorts and array of integers
+in ascending order using the bubble sort algorithm
+* @array: array of de input since main
+* @size: size of array
+**/
+void bubble_sort(int *array, size_t size)
+{
+    listint_t aux[];
+    int i, j;
+
+}
