68518db9077ab0a59a2e208f9241e4f7059695ec
towasme <towasm@gmail.com>
2020-02-03 17:36:30 -0500

    betty correction

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -1,37 +1,37 @@
 #include "sort.h"
 
 /**
- * selection_sort - sorts an array of integers in ascending order using the Selection sort algorithm
+ * selection_sort - sorts an array of integers in ascending order using the
+ * Selection sort algorithm
  * @array: array of data of input
  * @size: size of array
- * */
+ **/
 void selection_sort(int *array, size_t size)
 {
-    size_t i, j, idx, aux;
-    int flag = 0;
+	size_t i, j, idx, aux;
+	int flag = 0;
 
-    if (array == NULL || size <= 0)
-	    return;
+	if (array == NULL || size <= 0)
+		return;
 
-    for (i = 0; i < (size - 2); i++)
-    {
-        idx = i;
-        for (j = i + 1; j < size; j++)
-        {
-            if (array[j] < array[idx])
-            {
-                idx = j;
-                flag = 1;
-                /* printf("nuevo numero mas peque;o [%d]\n", array[idx]); */
-            }
-        }
-                    if (flag == 1)
-            {
-                aux = array[idx];
-                array[idx] = array[i];
-                array[i] = aux;
-                print_array(array, size);
-                flag = 0;
-            }
-    }
+	for (i = 0; i < (size - 2); i++)
+	{
+		idx = i;
+		for (j = i + 1; j < size; j++)
+		{
+			if (array[j] < array[idx])
+			{
+				idx = j;
+				flag = 1;
+			}
+		}
+		if (flag == 1)
+		{
+			aux = array[idx];
+			array[idx] = array[i];
+			array[i] = aux;
+			print_array(array, size);
+			flag = 0;
+		}
+	}
 }
