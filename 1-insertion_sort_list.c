44b70999aed1aad376778a21f65c070e554e81a2
andres0191 <andres_felipe0191@hotmail.com>
2020-02-04 18:24:41 +0000

    modify task 1 and update to file sorth.h

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,96 +1,58 @@
 #include "sort.h"
 /**
 * swap_2_nodes - function for swan in two nodes
-* @aux1: pointer of input
-* @aux2: pointer of input
-*
-* Return: result of swap in aux_2
+* @aux_2: pointer of input
 **/
-listint_t *swap_2_nodes(listint_t *aux_1, listint_t *aux_2)
+void swap_2_nodes(listint_t *aux_2)
 {
-	aux_2->prev = aux_1;
+	aux_2->next->prev = NULL;
+	aux_2->prev = aux_2->next;
 	aux_2->next = NULL;
-	aux_2->prev->prev = NULL;
 	aux_2->prev->next = aux_2;
-	while (aux_2->prev)
-		aux_2 = aux_2->prev;
-
-	return (aux_2);
 }
 
-/*
-* swap_last_node - function that swaps the last node
-* @aux_1: pointer to input
+/**
+* swap_last_node - function that swaps the last node of the array
 * @aux_2: pointer to input
-*
-* Return: the double linnked list starting from the header
 **/
-
-listint_t *swap_last_node(listint_t *aux_1, listint_t *aux_2)
+void swap_last_node(listint_t *aux_2)
 {
-	aux_1->next = aux_2;
-	aux_1->prev = aux_2->prev;
-	aux_2->prev->next = aux_1;
-	aux_2->prev = aux_1;
+	aux_2->prev->next = aux_2->next;
+	aux_2->next->next = aux_2;
+	aux_2->next->prev = aux_2->prev;
+	aux_2->prev = aux_2->next;
 	aux_2->next = NULL;
-	while (aux_2->prev)
-		aux_2 = aux_2->prev;
-
-	return (aux_2);
 }
 
 /**
  * swap_nodes - swaps two nodes form a double linked list
- * @aux_1: pointer to a node
  * @aux_2: pointer to the prevo=ious node
- *
- * Return: double linked list header
  */
-listint_t *swap_nodes(listint_t *aux_1, listint_t *aux_2)
+void swap_nodes(listint_t *aux_2)
 {
-	aux_2->next = aux_1->next;
-	aux_1->next->prev = aux_2;
-	aux_2->prev->next = aux_1;
-	aux_1->prev = aux_2->prev;
-	aux_2->prev = aux_1;
-	aux_1->next = aux_2;
-	while (aux_2->prev)
-	{
-		aux_2 = aux_2->prev;
-		printf("entra al swap\n");
-	}
-	printf("valor header lista=[%d]\n", aux_2->n);
-	return (aux_2);
+	aux_2->next->next->prev = aux_2;
+	aux_2->next->prev = aux_2->prev;
+	aux_2->prev = aux_2->next;
+	aux_2->next = aux_2->next->next;
+	aux_2->prev->prev->next = aux_2->prev;
+	aux_2->prev->next = aux_2;
 }
-
 /**
-* swap_nodes - swaps two nodes form a double linked list$
-* @aux_1: pointer to a node
+* swap_first_node - swaps two nodes form a double linked list
 * @aux_2: pointer to the prevo=ious node
-*
-* Return: double linked list header
 */
-listint_t *swap_first_node(listint_t *aux_1, listint_t *aux_2)
+void swap_first_node(listint_t *aux_2)
 {
-	/*hay q trocar los aux, porque aux 1 es 13(primer valor) y aux_2 es 19 (segundo valor)*/
-	printf("aux_2 = [[%d]]\n aux_1 = [[%d]]\n", aux_2->n, aux_1->n);
-	aux_2->next = aux_1->next;
-	aux_1->next = aux_2;
-	aux_1->next->prev = aux_2;
-	aux_2->prev = aux_1;
-	aux_1->prev = NULL;
-	while (aux_2)
-	{
-		aux_2 = aux_2->prev;
-		printf("entra al swap first node\n");
-	}
-	printf("valor header lista=[%d]\n", aux_2->n);
-	return (aux_2);
+	aux_2->prev = aux_2->next;
+	aux_2->next->next->prev = aux_2;
+	aux_2->next = aux_2->next->next;
+	aux_2->prev->prev = NULL;
+	aux_2->prev->next = aux_2;
 }
 
 /**
  * insertion_sort_list - sorts a double linked list in ascending order
- * @**list: double linked list unsorted
+ * @list: double linked list unsorted
  */
 
 void insertion_sort_list(listint_t **list)
@@ -102,36 +64,34 @@ void insertion_sort_list(listint_t **list)
 	{
 		while (aux_2 != NULL)
 		{
-			printf("valor aux_2= [%d]\n valor aux_1=[%d]\n", aux_2->n, aux_1->n);
 			if (aux_2->n > aux_1->n && aux_2->prev == NULL && aux_1->next == NULL)
 			{
-				*list = swap_2_nodes(aux_1, aux_2);
-				printf("entra al swap 2\n");
+				swap_2_nodes(aux_2);
+				*list = aux_2->prev;
 				print_list(*list);
 			}
-			if (aux_2->n > aux_1->n && aux_2->prev == NULL)
+			else if (aux_2->n > aux_1->n && aux_2->prev == NULL)
 			{
-				*list = swap_first_node(aux_1, aux_2);
+				swap_first_node(aux_2);
+				*list = aux_2->prev;
 				print_list(*list);
 			}
-			if (aux_2->n > aux_1->n && aux_2->prev != NULL)
+			else if (aux_2->n > aux_1->n && aux_2->prev != NULL)
 			{
-				if (aux_2->n > aux_1->n && aux_2->next == NULL)
+				if (aux_2->n > aux_1->n && aux_1->next == NULL)
 				{
-					*list = swap_last_node(aux_1, aux_2);
-					printf("entra a swap last node\n");
+					swap_last_node(aux_2);
 					print_list(*list);
 				}
 				else
 				{
-					*list = swap_nodes(aux_1, aux_2);
-					printf("entra a swap nodes\n");
+					swap_nodes(aux_2);
 					print_list(*list);
 				}
 			}
 			aux_2 = aux_2->prev;
 		}
-	aux_1 = aux_1->next;
-	aux_2 = aux_1;
+		aux_1 = aux_1->next;
+		aux_2 = aux_1;
 	}
 }
--- sort.h
+++ sort.h
@@ -24,4 +24,5 @@ void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
 
+
 #endif
