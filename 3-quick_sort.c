84795f73d19b084d41106b174f440d0b288ad330
andres0191 <andres_felipe0191@hotmail.com>
2020-02-04 21:41:50 +0000

    add task 3

---
+++ 3-quick_sort.c
@@ -0,0 +1,21 @@
+#include "sort.h"
+/**
+* quick_sort - sorts an array of integers in ascending order using the Quick sort algorithm
+* @array: array of input for organice with quick_sort function
+* @size: size of array
+**/
+void quick_sort(int *array, size_t size)
+{
+    int pivot;
+    size_t i = 0;
+
+    pivot =  array;
+    if (size > 1)
+    {
+        for (i = 0; array[i] != NULL; i++)
+        {
+
+        }
+    }
+}
+void partition(int *array, size_t )
