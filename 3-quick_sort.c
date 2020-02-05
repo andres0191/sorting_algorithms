baf1bbf473ab2969fd0702673af9837da962d64f
towasme <towasm@gmail.com>
2020-02-05 19:15:36 +0000

    added conditionals fot when the two arrays are the same number

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -1,55 +1,51 @@
 #include "sort.h"
 /**
- * partition - function thath realize partition the array
- * @array: array of the input
- * @low: size low of array
- * @high: size high of array
- *
- * Return: Always i + 1
- **/
+* partition - function thath realize partition the array
+* @array: array of the input
+* @low: size low of array
+* @high: size high of array
+* @size: size of the array
+* Return: Always i + 1
+**/
 int partition(int *array, int low, int high, size_t size)
 {
-	int pivot = array[high], i = (low - 1), j = 0;
+	int pivot = array[high], i = (low), j;
 	int temp = 0;
 
-	for (j = low; j <= high - 1; j++)
+	for (j = low; j < high; j++)
 	{
-		if (array[j] <= pivot)
+		if (array[j] < pivot)
 		{
+			if (array[j] != array[i])
+			{
+				temp = array[i];
+				array[i] = array[j];
+				array[j] = temp;
+				print_array(array, size);
+			}
 			i++;
-			temp = array[i];
-			array[i] = array[j];
-			array[j] = temp;
-			print_array(array, size);
-/*
-			printf("array[j]=[%d], array[i]=[%d], pivot=[%d]\n", array[j], array[i], pivot);
-*/
 		}
 	}
-/*
-	if (pivot < array[i + 1] && array[i + 1] != array[j])
+	if (array[high] != array[i])
 	{
-*/
-	temp = array[i + 1];
-	array[i + 1] = array[high];
-	array[high] = temp;
-	print_array(array, size);
-/*
-	printf("----array[j]=[%d], array[i]=[%d], pivot=[%d]\n", array[j], array[i], pivot);
-*/
-	return (i + 1);
+		temp = array[i];
+		array[i] = array[high];
+		array[high] = temp;
+		print_array(array, size);
+	}
+	return (i);
 }
 
 /**
- * recurtion - function that execute recursion in the porject
+* recursion - function that execute recursion in the porject
 * @array: array of the input
- * @low: size low of array
- * @high: size high of array
- * @size: size of array
- **/
+* @low: size low of array
+* @high: size high of array
+* @size: size of array
+**/
 void recursion(int *array, int low, int high, size_t size)
 {
-	int i = 0;
+	int i;
 
 	if (low < high)
 	{
@@ -66,7 +62,7 @@ void recursion(int *array, int low, int high, size_t size)
 **/
 void quick_sort(int *array, size_t size)
 {
-	int low = 0, high = size - 1;
+	int low = 0, high = (size - 1);
 
 	if (array != NULL && size != 0)
 		recursion(array, low, high, size);
